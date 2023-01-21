2aa817a9c61c07a895544fca9e1415c224a8f5f1
Pau <pau.recort@bsc.es>
2023-01-21 12:27:15 -0500

    Add block par to API

--- API/api.h
+++ API/api.h
@@ -1,13 +1,26 @@
 #ifndef API_H
 #define API_H
 
+#ifndef ST_MAX
+#define ST_MAX 32
+#endif
+
+#define ST_FRAME_SIZE 128
+
+#ifndef ST_STACK_SIZE
+#define ST_STACK_SIZE 128
+#endif
+
+char st_frame[ST_MAX][ST_FRAME_SIZE]={1};
+char st_stack[ST_MAX][ST_STACK_SIZE]={1};
+char st_busy[ST_MAX]={1};
+
 
 //////////// Internal functions ////////////
 
 void fork_sync(int a0, int a1, int a2, int a3, int a4, void *child_func, char *child_frame, char *child_stack);
 
 int wait_sync(char *child_frame);
-
 
 //////////// Full feature fork ////////////
 
@@ -56,5 +69,81 @@ inline void fork5NS(int a0, int a1, int a2, int a3, int a4, void *child_func, ch
 inline int wait(char *child_frame)
 	{return wait_sync(child_frame);}
 
+//////////// Block ////////////
+
+int block1D(int x0, int x1, int dx, int (*shader_func)(int), int (*reduce_func)(int,int)) {
+
+	int res = 0;
+	int sid = 0;
+
+	for(int i = 0; i < ST_MAX; ++i)
+		st_busy[i] = 0;
+
+	for (int i=x0; i<x1; i+=dx) {
+		if(st_busy[sid])
+			res = (*reduce_func)(res,wait(st_frame[sid]));
+		fork1(i,(void*)shader_func,st_frame[sid],st_stack[sid]+ST_STACK_SIZE);
+		st_busy[sid] = 1; sid = (sid+1)%ST_MAX;
+	}
+
+	for(sid = 0; sid < ST_MAX; ++sid)
+		if(st_busy[sid])
+			res = (*reduce_func)(res,wait(st_frame[sid]));
+
+	return res;
+
+}
+
+int block2D(int x0, int x1, int dx, int y0, int y1, int dy, int (*shader_func)(int,int), int (*reduce_func)(int,int)) {
+
+	int res = 0;
+	int sid = 0;
+
+	for(int i = 0; i < ST_MAX; ++i)
+		st_busy[i] = 0;
+
+	for (int i=x0; i<x1; i+=dx) {
+		for (int j=y0; j<y1; j+=dy) {
+			if(st_busy[sid])
+				res = (*reduce_func)(res,wait(st_frame[sid]));
+			fork2(i,j,(void*)shader_func,st_frame[sid],st_stack[sid]+ST_STACK_SIZE);
+			st_busy[sid] = 1; sid = (sid+1)%ST_MAX;
+		}
+	}
+
+	for(sid = 0; sid < ST_MAX; ++sid)
+		if(st_busy[sid])
+			res = (*reduce_func)(res,wait(st_frame[sid]));
+
+	return res;
+
+}
+
+int block3D(int x0, int x1, int dx, int y0, int y1, int dy, int z0, int z1, int dz, int (*shader_func)(int,int,int), int (*reduce_func)(int,int)) {
+
+	int res = 0;
+	int sid = 0;
+
+	for(int i = 0; i < ST_MAX; ++i)
+		st_busy[i] = 0;
+
+	for (int i=x0; i<x1; i+=dx) {
+		for (int j=y0; j<y1; j+=dy) {
+			for (int k=z0; k<z1; k+=dz) {
+				if(st_busy[sid])
+					res = (*reduce_func)(res,wait(st_frame[sid]));
+				fork3(i,j,k,(void*)shader_func,st_frame[sid],st_stack[sid]+ST_STACK_SIZE);
+				st_busy[sid] = 1; sid = (sid+1)%ST_MAX;
+			}
+		}
+	}
+
+	for(sid = 0; sid < ST_MAX; ++sid)
+		if(st_busy[sid])
+			res = (*reduce_func)(res,wait(st_frame[sid]));
+
+	return res;
+
+}
 
 #endif
--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,32 +1,24 @@
+#define ST_MAX 64
+#define ST_STACK_SIZE 128
+
 #include"../API/api.h"
 #include"../MMIO/mmio.h"
-
-#define MAX_THD 32
-#define FRAME_SIZE 128
-#define STACK_SIZE 128
 
 #define BASE 8192
 #define STEP 512
 #define MAXITER 100
 #define INFTY 4*BASE
 
-char child_stack[MAX_THD][STACK_SIZE]={1};
-char child_frame[MAX_THD][FRAME_SIZE]={1};
-int busy[MAX_THD]={1};
-
 typedef struct {
 	int r;
 	int i;
 } Complex;
 
-inline int fromint(int a) {
-	return BASE*a;
-}
+inline int fromint(int a)
+	{ return BASE*a; }
 
-inline Complex com(int r, int i) {
-	Complex res = {r,i};
-	return res;
-}
+inline Complex com(int r, int i)
+	{ Complex res = {r,i}; return res; }
 
 inline Complex add(Complex a, Complex b)
 	{return com(a.r+b.r,a.i+b.i);}
@@ -40,7 +32,8 @@ inline Complex mul(Complex a, Complex b)
 inline int modsq(Complex a)
 	{return (a.r*a.r+a.i*a.i)/BASE;}
 
-int inside(Complex c) {
+int inside(int r, int i) {
+	Complex c = {r,i};
 	Complex z = {0,0};
 	for(int i = 0; i < MAXITER; ++i)
 		if(modsq(z) > INFTY)
@@ -52,58 +45,16 @@ int inside(Complex c) {
 
 int mandelbrot(int l, int r, int b, int t) {
 	int total = 0;
-	for (int i = b; i < t; i+=STEP)
-    	for (int j = l; j < r; j+=STEP)
-    		total += inside(com(j,i));
+	for (int i = l; i < r; i+=STEP)
+    	for (int j = b; j < t; j+=STEP)
+    		total += inside(i,j);
     return total;
 }
 
-int mandelbrot4(int l, int r, int b, int t) {
-
-	int total = 0;
-
-	int h = l+(r-l)*9/16;
-	int v = b+(t-b)*8/16;
-
-	fork4(l,h,b,v,(void*)mandelbrot,child_frame[0],child_stack[0]+STACK_SIZE);
-	fork4(l,h,v,t,(void*)mandelbrot,child_frame[1],child_stack[1]+STACK_SIZE);
-	fork4(h,r,b,v,(void*)mandelbrot,child_frame[2],child_stack[2]+STACK_SIZE);
-	fork4(h,r,v,t,(void*)mandelbrot,child_frame[3],child_stack[3]+STACK_SIZE);
-
-	total+=wait(child_frame[0]);
-    total+=wait(child_frame[1]);
-    total+=wait(child_frame[2]);
-    total+=wait(child_frame[3]);
-
-    return total;
-
+int mandelbrotN (int l, int r, int b, int t) {
+	int sum (int a, int b) {return a+b;};
+	return block2D(l,r,STEP,b,t,STEP,&inside,&sum);
 }
-
-int mandelbrotN(int l, int r, int b, int t) {
-
-	int thd   = 0;
-	int total = 0;
-
-	for(thd = 0; thd < MAX_THD; ++thd)
-    	busy[thd] = 0;
-	
-	thd = 0;
-	for (int i = b; i < t; i+=STEP) {
-    	for (int j = l; j < r; j+=STEP) {
-    		if(busy[thd])
-    			{total += wait(child_frame[thd]); busy[thd] = 0;}
-    		fork2(j,i,(void*)inside,child_frame[thd],child_stack[thd]+STACK_SIZE); busy[thd] = 1;
-    		thd = (thd+1)%MAX_THD;
-    	}
-    }
-
-    for(thd = 0; thd < MAX_THD; ++thd)
-    	if(busy[thd])
-	    	total += wait(child_frame[thd]);
-
-    return total;
-}
-
 
 int main() {
 
